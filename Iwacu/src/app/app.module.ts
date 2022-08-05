7ef36f4cc5ec57d23299d849a5a5165b69c5b40a
Yvon Gatete <gateteb3@gmail.com>
2022-08-04 21:49:28 -0400

    optimization / some sign up error remains

--- Iwacu/src/app/app.module.ts
+++ Iwacu/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http"
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -12,38 +10,31 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PostListComponent } from './posts/post-list/post-list.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from 'src/app/auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { ErrorComponent } from './error/error.component';
 import { AuthInterceptor } from './interceptors/auth-interceptor'
 import { ErrorInterceptor } from './interceptors/error-interceptor';
 import { AngularMaterialModule } from "./angular-material.module";
+import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PostCreateComponent,
-    PostListComponent,
     HeaderComponent,
-    LoginComponent,
-    SignupComponent,
     ErrorComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     FlexLayoutModule,
-    FormsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    PostsModule,
+    AuthModule
 
   ],
   providers: [
